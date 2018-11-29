13d4eebb6d19d78eccc4bd0e685206952a986e6c
Chris Eddy <ceddy.zen@gmail.com>
2018-11-28 22:42:25 -0800

    unfortunate typo

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.get('/*', express.static(path.join(__dirname, '../..', 'build')));
+app.get('/*', express.static(path.join(__dirname, '../', 'build')));
 
 const googleTrends = require('google-trends-api');
 
